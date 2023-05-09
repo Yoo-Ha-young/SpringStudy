110cf5caef685e585f1517b3fc46917a3732044e
Yoo-Ha-Young <bluekitten74@naver.com>
2023-05-10 03:43:53 +0900

    spring-study-first

---
+++ spring-study-first/src/test/java/dev/SpringStudy/springstudyfirst/SpringStudyFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.SpringStudy.springstudyfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
